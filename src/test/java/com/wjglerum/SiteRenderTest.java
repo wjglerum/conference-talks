a7574e3cefa00f552e93da424bba19e8a7a599bb
Willem Jan Glerum <wjglerum@gmail.com>
2026-06-21 10:32:21 +0200

    Rewrite homepage as setlist with tour sections and themed map

--- src/test/java/com/wjglerum/SiteRenderTest.java
+++ src/test/java/com/wjglerum/SiteRenderTest.java
@@ -32,4 +32,38 @@ class SiteRenderTest {
             .statusCode(200)
             .body(containsString("id=\"theme-toggle\""));
     }
+
+    @Test
+    void homepageShowsTourSections() {
+        given().when().get("/").then()
+            .statusCode(200)
+            .body(containsString("Now Playing"))
+            .body(containsString("Past Tours"))
+            .body(containsString("Singles"));
+    }
+
+    @Test
+    void homepageShowsActiveTourWithAnchorAndCount() {
+        given().when().get("/").then()
+            .statusCode(200)
+            .body(containsString("id=\"tour-concurrency-crossroads\""))
+            .body(containsString("Concurrency Crossroads"))
+            .body(containsString("Now touring"));
+    }
+
+    @Test
+    void homepageShowsWrappedTour() {
+        given().when().get("/").then()
+            .statusCode(200)
+            .body(containsString("id=\"tour-sso-quarkus-oidc\""))
+            .body(containsString("SSO made easy with Quarkus OIDC"));
+    }
+
+    @Test
+    void homepageRendersComputedStats() {
+        given().when().get("/").then()
+            .statusCode(200)
+            .body(containsString("<span class=\"num\">36</span>"))
+            .body(containsString("Shows"));
+    }
 }
